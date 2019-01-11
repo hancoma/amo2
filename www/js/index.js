ee3d177bdf276628bf33bdb46ba7c90c0271ada8
hancoma <hancoma@gmail.com>
2019-01-12 00:32:44 +0900

    Update index.js
    수정

--- www/js/index.js
+++ www/js/index.js
@@ -128,19 +128,7 @@ push.on('error', function(e) {
 };
   
   function checkconnection(){ alert(navigator.onLine);} 
-  function CheckConnection() 
-{ 
-    if(!navigator.network) 
-    { 
-     // set the parent windows navigator network object to the child window 
-     navigator.network = window.top.navigator.network; 
-    } 
-
-    // return the type of connection found 
-    return ((navigator.network.connection.type === "none" || navigator.network.connection.type === null || 
-      navigator.network.connection.type === "unknown") ? false : true); 
-} 
-
+  
 
 
 function save_reg_id(reg_id) {
