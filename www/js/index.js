b9954ba69572d1d65e76e866c65790e823754659
hancoma <hancoma@gmail.com>
2019-01-12 00:21:10 +0900

    Update index.js
    네트워크 채크

--- www/js/index.js
+++ www/js/index.js
@@ -61,6 +61,7 @@ var app = {
     },
 
     onmain : function() {
+        checkconnection(); 
 document.addEventListener("backbutton", exit_show, false); 
          var reg_id=device.uuid;
        // 기기 번호 검출 
@@ -126,6 +127,20 @@ push.on('error', function(e) {
 
 };
   
+  function checkconnection(){ alert(navigator.onLine);} 
+  function CheckConnection() 
+{ 
+    if(!navigator.network) 
+    { 
+     // set the parent windows navigator network object to the child window 
+     navigator.network = window.top.navigator.network; 
+    } 
+
+    // return the type of connection found 
+    return ((navigator.network.connection.type === "none" || navigator.network.connection.type === null || 
+      navigator.network.connection.type === "unknown") ? false : true); 
+} 
+
 
 
 function save_reg_id(reg_id) {
